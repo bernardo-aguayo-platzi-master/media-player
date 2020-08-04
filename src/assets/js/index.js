5f95ef98d72f11bdb2c3e95313f027cfe31dd85c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-04 12:48:51 -0500

    apply module scope

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,21 +1,7 @@
+import MediaPlayer from "./mediaPlayer.js"
+
 const VIDEO = document.querySelector("video");
 const BUTTON = document.querySelector("button");
-
-function MediaPlayer(config) {
-    this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function() {
-    this.media.play();
-}
-
-MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-}
-
-MediaPlayer.prototype.togglePlay = function() {
-    (this.media.paused) ? this.play(): this.pause()
-}
 
 const PLAYER = new MediaPlayer({
     el: VIDEO
